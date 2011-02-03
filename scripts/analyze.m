3ba2a174c560f1187d7de20aa483feedd9eaf6a8
Arkajit Dey <arkajit.dey@gmail.com>
2011-02-03 17:31:22 -0500

    adding data from a test run of TrackAnalyzer functions

--- scripts/analyze.m
+++ scripts/analyze.m
@@ -2,6 +2,7 @@ ta = TrackAnalyzer(30, ... % number of trials
                    20, ... % number of bins
                     5);    % number of transitions used in random trajectories
 
-errS = ta.analyzeLength(100, 5, 1);
-errTau = ta.analyzeTau(100, 2, 0.1:0.1:5);
-errRat = ta.analyzeRatio(100, 0:0.1:5, 1);
+nSteps = 100;
+errS = ta.analyzeLength(nSteps, 5, 1);
+errTau = ta.analyzeTau(nSteps, 2, 0.1:0.1:5);
+errRat = ta.analyzeRatio(nSteps, 0:0.1:5, 1);
