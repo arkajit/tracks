ba8795000ca3d21059316f78c6ad554d33ef1f66
Arkajit Dey <arkajit.dey@gmail.com>
2011-03-11 06:57:41 -0500

    updating chromosomes script to use real timestep. new data.

--- scripts/chromosomes.m
+++ scripts/chromosomes.m
@@ -3,6 +3,8 @@ data = importdata('../data/chromosome_tracks.mat');
 Dmax = 0.1;
 Vmax = 0.2;
 nBins = 10;
+odds = 25;
+tau = data.info.timestep;
 tracknos = data.tracks(:,1);
 n = max(tracknos);
 
@@ -10,13 +12,13 @@ trajectories = cell(n, 1);
 tracks = cell(n, 1);
 D = cell(n, 1);
 V = cell(n, 1);
-hmmSolver = HMMSolver(Dmax, Vmax, nBins);
+hmmSolver = HMMSolver(Dmax, Vmax, nBins, odds);
 
 for i=1:n
 	disp(sprintf('Analyzing Track %d...', i));
 	savedir = sprintf('../ctfigs/%d', i);
 	trajectories{i} = data.tracks(find(tracknos==i), 3:5);
-	tracks{i} = Track.fromTrajectory(trajectories{i}, 1);
+	tracks{i} = Track.fromTrajectory(trajectories{i}, tau);
 	[D{i}, V{i}] = hmmSolver.solve(tracks{i});
 	plotter = TrackPlotter(tracks{i}, savedir);
 	plotter.plotDiffusion(D{i});
