9aeae82866c6f6d42300b7a35833af62b7146cab
Arkajit Dey <arkajit.dey@gmail.com>
2011-03-14 10:21:30 -0400

    identified pieces in Estep that need to be changed for continuous HMM EM

--- fhmm/em_hmm.m
+++ fhmm/em_hmm.m
@@ -1,3 +1,5 @@
+% TODO(arkajit): update this to work with continuous Gaussian emissions
+
 function [hmm,bic] = em_hmm(X,hmm,verbose)
 
 if (nargin<3), verbose = 0; end; 
@@ -23,7 +25,7 @@ ndata = 0; for i=1:length(X), ndata = ndata + length(X{i}); end; %summing up the
 %counting the number of independent parameter d
 %initial probalilities t has k parameters, but they have to sum to 1 => k-1 independent parameters
 %transition matrix T has k x k entries, but each row has to sum to 1 => k*(k-1) independent parameters
-%emition probabilities E is a k x l matrix, but each row has to sum to 1 => k*(l-1) independent parameters
+%emission probabilities E is a k x l matrix, but each row has to sum to 1 => k*(l-1) independent parameters
 
 bic = log_lik - (k-1+k*(k-1)+k*(l-1))/2*log(ndata); 
 
@@ -54,28 +56,34 @@ function [Nt,NT,NE,log_lik] = Estep(X,hmm)
 % NE(s,z) = expected number of of times we were in s and generated z
 Nt = zeros(size(hmm.log_t)); 
 NT = zeros(size(hmm.log_T)); 
-NE = zeros(size(hmm.log_E)); 
+%NE = zeros(size(hmm.log_E)); % can't take the size of a function 
 
 k = size(hmm.log_t,1); % number of states
 
 log_lik = 0; 
-for i=1:length(X),
+for i=1:length(X), 	% for each data example
     x = X{i}; m = length(x); 
     
     log_a = forward(x,hmm); % forward probabilities on a log-scale
     log_b = backward(x,hmm);% backward probabilities on a log-scale
-    log_lik = log_lik + log_sum_exp(log_a(:,end),1);
+    log_lik = log_lik + log_sum_exp(log_a(:,end),1); % add Pr(Data)
 
     % accumulate posterior counts 
     Nt = Nt + norm_exp(log_a(:,1)+log_b(:,1)); 
-    for j=1:m-1,
-        NE(:,x(j)) = NE(:,x(j)) + norm_exp(log_a(:,j)+log_b(:,j)); 
+    for j=1:m-1,	% for each timepoint in an example
+    %    NE(:,x(j)) = NE(:,x(j)) + norm_exp(log_a(:,j)+log_b(:,j)); 
     
         log_A = repmat(log_a(:,j),1,k);
-        log_B = repmat(log_b(:,j+1),1,k); 
-        log_E = repmat(hmm.log_E(:,x(j+1)),1,k);
+        log_B = repmat(log_b(:,j+1),1,k);
+
+				log_e = zeros(k,1);
+				for s=1:k
+					log_e(s) = hmm.log_E(s, x(j+1));
+				end
+
+				log_E = repmat(log_e, 1, k);
         NT = NT + norm_exp(log_A+hmm.log_T+log_E'+log_B'); 
     end;
-    NE(:,x(m)) = NE(:,x(m)) + norm_exp(log_a(:,m)+log_b(:,m));
+    %NE(:,x(m)) = NE(:,x(m)) + norm_exp(log_a(:,m)+log_b(:,m));
 
 end;
