1522a8008274778c38a0b47acf6c3950ad86c073
Arkajit Dey <arkajit.dey@gmail.com>
2011-03-23 10:50:31 -0400

    rewriting em_fhmm for a single training example

--- fhmm/em_fhmm.m
+++ fhmm/em_fhmm.m
@@ -1,52 +1,49 @@
-function [hmm] = em_fhmm(X, hmm)
+function [hmm] = em_fhmm(x, hmm)
 % em_fhmm - Expectation Maximization for a continuous Gaussian HMM
-%		X - cell array of observed emissions, each vector one training example
+%		x - mx1 vector of observations (a single training example)
 %		hmm - initial guess for the parameters
 %			requires t, T, means, stddevs fields to be set to init values
 %			returns the MLE of hmm parameters including t, T, means, stddevs
 
-[hmm, L] = one_em_iter(X, hmm);
+[hmm, L] = one_em_iter(x, hmm);
 
 L0 = L-abs(L);
 while (L-L0 > abs(L)*1e-6)
 	L0 = L;
-	[hmm, L] = one_em_iter(X, hmm);
+	[hmm, L] = one_em_iter(x, hmm);
 end
 
 hmm.loglik = L;
 
 % -----------------------------------------
-function [hmm, loglik] = one_em_iter(X, hmm)
-	N = length(X); % number of training examples
-	S = length(hmm.t); % number of states
+function [hmm, loglik] = one_em_iter(x, hmm)
+	S = length(hmm.t); 		% number of states
+	m = length(x); 				% number of timesteps in hidden chain
 
-	Nt = zeros(S, 1);
-	NT = zeros(S, S);
-	means = zeros(S, 1);
-	vars = zeros(S, 1);
-	W = zeros(S, 
+	Nt = zeros(S, 1);			% counts of initial states
+	NT = zeros(S, S);			% counts of transitions
+	W = zeros(S, m);			% weights to update mean and variances
 
 	%% E-Step
-	for i=1:N
-		x = X{i};	% particular vector of emissions (DATA)
-		m = length(x); % number of timesteps in hidden chain
-		log_a = forward(x, hmm);
-		log_b = backward(x, hmm);
-		loglik = loglik + log_sum_exp(log_a(:,end),1); % add log Pr(DATA)
+	log_a = forward(x, hmm);
+	log_b = backward(x, hmm);
+	loglik = log_sum_exp(log_a(:,end),1); 		% log Pr(DATA)
 
-		% update initial state counts	
-    Nt = Nt + norm_exp(log_a(:,1)+log_b(:,1));
+	% calculate update weights
+	log_w = log_a + log_b - loglik;
+	W = exp(log_w);
+	W = W ./ repmat(sum(W, 2), 1, m); 				% normalize weights
 
-		% update transition counts
-    for t=1:m-1	
-			log_A = repmat(log_a(:,t),1,k);
-      log_B = repmat(log_b(:,t+1),1,k);
-			log_E = repmat(hmm.logEall, 1, k);
-			NT = NT + norm_exp(log_A+hmm.log_T+log_E'+log_B'); 
-    end
+	% update initial state counts	
+	Nt = Nt + norm_exp(log_a(:,1)+log_b(:,1));
 
-		
-	end % end LOOP over training examples
+	% update transition counts
+	for t=1:m-1	
+		log_A = repmat(log_a(:,t),1,k);
+		log_B = repmat(log_b(:,t+1),1,k);
+		log_E = repmat(hmm.logEall, 1, k);
+		NT = NT + norm_exp(log_A+hmm.log_T+log_E'+log_B'); 
+	end
 
 	%% M-Step
 	hmm.t = Nt / sum(Nt);	
@@ -54,8 +51,7 @@ function [hmm, loglik] = one_em_iter(X, hmm)
 
 	hmm.T = NT ./ repmat(sum(NT, 2), 1, S);
 	hmm.log_T = log(hmm.T);
+
+	hmm.means = W * x;
+	hmm.stddevs = sqrt(W * ((x - hmm.means).^2));
 end
-
-%function [hmm, loglik] = one_example(x, hmm)
-
-%end
